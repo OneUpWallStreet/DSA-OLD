bff7c3036b039e0a01627e6bf6bd31cd6db5356e
OneUpWallStreet <oneupwallstreet@gmail.com>
2021-07-21 17:28:56 +0530

    Added second case to example

--- ExampleProblem_1.swift
+++ ExampleProblem_1.swift
@@ -1,9 +1,13 @@
 
 //Given a Array and a integer, find if there are any 2 numbers in the array such that their sum is that interger
 
+// In the first case the array is sorted and in the second case the array is unsorted so we have to solve it using a Hash Set.
+
 //Problem is from https://youtu.be/XKu_SEDAykw  -> How to: Work at Google — Example Coding/Engineering Interview
 
 var array: Array<Int> = [1,3,3,4]
+var complements = Set<Int>()
+var unsorted_array: Array<Int> = [1,7,4,3,7,0,7]
 
 struct answer {
     
@@ -12,7 +16,25 @@ struct answer {
     
 }
 
-print(array.count)
+func solve_unsorted(arr: Array<Int>,sumToFind: Int) -> answer{
+    
+    
+    for index in 0..<unsorted_array.count{
+        
+        let current_value = unsorted_array[index]
+        let complement = sumToFind - current_value
+        
+        
+        if complements.contains(complement){
+            return answer(isFound: true, matchids: (value1: current_value, value2: complement))
+        }
+        else{
+            complements.insert(complement)
+        }
+    }
+    return answer(isFound: false)
+}
+
 
 func solve(arr: Array<Int>,sumToFind: Int) -> answer{
     
@@ -43,7 +65,7 @@ func solve(arr: Array<Int>,sumToFind: Int) -> answer{
 
 
 let answer = solve(arr: array, sumToFind: 4)
+let answer_2 = solve_unsorted(arr: unsorted_array, sumToFind: 8)
 
-print(answer)
 
 
